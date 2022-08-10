a9e6f00b6c99b0b37a05b014645474e341d6e895
jordanlesson <jordantreylesson@gmail.com>
2022-08-10 14:50:52 -0400

    Hardhat Implemented

--- contracts/Slashing.sol
+++ contracts/Slashing.sol
@@ -1,5 +1,5 @@
-// // SPDX-License-Identifier: MIT
-// pragma solidity >=0.8.15;
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.9;
 
 // import "./Validation.sol";
 // import "./Delegation.sol";
